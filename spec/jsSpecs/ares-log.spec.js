b6c8440f4f6a017411cfa4456f00c1c37eaa36c4
kclod kim <93501920+kclod-kim@users.noreply.github.com>
2024-01-25 14:28:37 +0900

    Fix a bug unit test of ares-log
    :Release Notes:
    Fix a bug unit test of ares-log
    
    :Detailed Notes:
    Fixed fail unit test of in ares-log when running unit test on OSE Emulator
    
    :Testing Performed:
    1. Checked CLI unit test on OSE(rpi4)
    2. Checked CLI unit test on OSE Emulator
    3. Checked API unit test
    4. Checked eslint
    
    :Issues Addressed:
    [WRQ-9608] Develop integrated ares-log

--- spec/jsSpecs/ares-log.spec.js
+++ spec/jsSpecs/ares-log.spec.js
@@ -685,8 +685,12 @@ describe(aresCmd + " negative tc", function() {
         exec(cmd + ` -n 2 -s invalid/aaa.log`, function(error, stdout, stderr) {
             if (stderr && stderr.length > 0) {
                 common.detectNodeMessage(stderr);
-                expect(stderr).toContain("ares-log ERR! [syscall failure]: ENOENT: no such file or directory");
-                expect(stderr).toContain("ares-log ERR! [Tips]: Please check if the path is valid");
+                if (options.device === "emulator") {
+                    expect(stderr).toContain("ares-log ERR! [Tips]: Unable to connect to the target device. root access required");
+                } else {
+                    expect(stderr).toContain("ares-log ERR! [syscall failure]: ENOENT: no such file or directory");
+                    expect(stderr).toContain("ares-log ERR! [Tips]: Please check if the path is valid");
+                }
             }
             done();
         });
